722131ab7ba41a092856b51b2768a4a545989e0b
Jordan Machalek <jdm4344@rit.edu>
2019-04-17 16:09:40 -0400

    Added some basic functionality to the client

--- client/client.js
+++ client/client.js
@@ -1,70 +1,128 @@
+let manual = true;
+
 const handleError = (message) => {
     $("#errorMessage").text(message);
+};
+  
+const sendAjax = (action, data) => {
+  $.ajax({
+    cache: false,
+    type: "POST",
+    url: action,
+    data: data,
+    dataType: "json",
+    success: (result, status, xhr) => {
+
+      window.location = result.redirect;
+    },
+    error: (xhr, status, error) => {
+      const messageObj = JSON.parse(xhr.responseText);
+
+      handleError(messageObj.error);
+    }
+  });        
+};
+
+
+// Submits data for the manual form
+const manualSubmit = (e) => {
+  e.preventDefault();
+
+  if($("#inputValue").val() == '') {
+    handleError("Input is required for submission");
+    return false;
   }
-  
-  const sendAjax = (action, data) => {
-    $.ajax({
-      cache: false,
-      type: "POST",
-      url: action,
-      data: data,
-      dataType: "json",
-      success: (result, status, xhr) => {
-  
-        window.location = result.redirect;
-      },
-      error: (xhr, status, error) => {
-        const messageObj = JSON.parse(xhr.responseText);
-  
-        handleError(messageObj.error);
-      }
-    });        
+
+  // TODO: validation/conversion  
+
+  sendAjax($("#manualForm").attr("action"), $("#manualForm").serialize());
+
+  return false;
+};
+
+// Submits data for the efficient form
+const efficientSubmit = (e) => {
+  e.preventDefault();
+
+  if($("#inputValue").val() == '') {
+    handleError("Input is required for submission");
+    return false;
   }
+
+  // TODO: validation/conversion
+
+  sendAjax($("#efficientForm").attr("action"), $("#efficientForm").serialize());
+
+  return false;
+};
   
-  $(document).ready(() => {
-    // Submits data for the manual form
-    $("#manualForm").on("submit", (e) => {
-      e.preventDefault();
-  
-      if($("#inputValue").val() == '') {
-        handleError("Input is required for submission");
-        return false;
-      }
-  
-      
-  
-      sendAjax($("#manualForm").attr("action"), $("#manualForm").serialize());
-  
-      return false;
-    });
+$(document).ready(() => {
+  // Setup default event handler for the manual submission form
+  $("#manualForm").on("submit", manualSubmit);  
 
-    // Submits data for the efficient form
-    $("#efficientForm").on("submit", (e) => {
-        e.preventDefault();
-    
-        if($("#inputValue").val() == '') {
-          handleError("Input is required for submission");
-          return false;
-        }
-    
-        
-    
-        sendAjax($("#efficientForm").attr("action"), $("#efficientForm").serialize());
-    
-        return false;
-    });
+  // Changes between displaying the manual and the efficient forms on the page
+  $("#changeFormButton").click(() => {
+    // console.log("change button clicked");
+    // console.log(manual);
+    if(manual) {
+      $('#changeFormButton').val("Do It the Easy Way");
+      manual = false;
 
-    // Changes between displaying the manual and the efficient forms on the page
-    $("#changeFormButton").on('click', (e) => {
-        $('#changeFormButton').text = "Do It the Hard Way";
-        $('#changeFormButton').text = "Do It the Easy Way";
-    });
+      $('#input').html(`<h1>The Efficient Way:</h1>
+      <form id="efficientForm" name="efficientForm" action="/appendValue" method="POST">
+        <label for="inputValue">Enter 8 Binary Digits: </label>
+        <input id="inputValue" type="number" name="inputValue" value="" size="8">
+        <input class="formSubmit" type="submit" value="Send Data">
+      </form>`);
 
-    $("#zeroButton").on('click', (e) => {
+      // Attach event handler for the efficient form
+      $("#efficientForm").on("submit", efficientSubmit);
+    }
+    else {
+      $('#changeFormButton').val("Do It the Hard Way");
+      manual = true;
 
-    });
+      $('#input').html(`<h1>The Intended Way:</h1>
+      <form id="manualForm" name="manualForm" action="/appendValue" method="POST">
+        <input id="zeroButton" type="button" value="0">
+        <input id="oneButton" type="button" value="1">
+        <label for="inputValue">Enter 8 Binary Digits: </label>
+        <input id="inputValue" type="number" name="inputValue" value="00000000" size="8" disabled>
+        <input class="formSubmit" type="submit" value="Send Data">
+      </form>`);
 
-    $("#oneButton").on('click', (e) => {
-
-    });
+      // Attach event handler for the manual form
+      $("#manualForm").on("submit", manualSubmit);
+    }
   });
+
+  $("#zeroButton").click(() => {
+    console.log("zero clicked");
+    const input = $('#inputValue');
+    let inputValue = input.val();
+
+    // If length < 8, append
+    if(inputValue.length < 8) {
+      input.val(inputValue.concat("0"));
+    }
+    else { 
+      // else, reset the value
+      input.val("");
+    }
+  });
+
+  $("#oneButton").click(() => {
+    console.log("one clicked");
+    const input = $('#inputValue');
+    let inputValue = $('#inputValue').val();
+
+    // If length < 8, append
+    if(inputValue.length < 8) {
+      input.val(inputValue.concat("1"));
+    }
+    else { 
+      // else, reset the value
+      input.val("");
+    }
+  });
+});
--- hosted/bundle.js
+++ hosted/bundle.js
@@ -1,64 +1,114 @@
 "use strict";
 
+var manual = true;
+
 var handleError = function handleError(message) {
-    $("#errorMessage").text(message);
+  $("#errorMessage").text(message);
 };
 
 var sendAjax = function sendAjax(action, data) {
-    $.ajax({
-        cache: false,
-        type: "POST",
-        url: action,
-        data: data,
-        dataType: "json",
-        success: function success(result, status, xhr) {
+  $.ajax({
+    cache: false,
+    type: "POST",
+    url: action,
+    data: data,
+    dataType: "json",
+    success: function success(result, status, xhr) {
 
-            window.location = result.redirect;
-        },
-        error: function error(xhr, status, _error) {
-            var messageObj = JSON.parse(xhr.responseText);
+      window.location = result.redirect;
+    },
+    error: function error(xhr, status, _error) {
+      var messageObj = JSON.parse(xhr.responseText);
 
-            handleError(messageObj.error);
-        }
-    });
+      handleError(messageObj.error);
+    }
+  });
+};
+
+// Submits data for the manual form
+var manualSubmit = function manualSubmit(e) {
+  e.preventDefault();
+
+  if ($("#inputValue").val() == '') {
+    handleError("Input is required for submission");
+    return false;
+  }
+
+  // TODO: validation/conversion  
+
+  sendAjax($("#manualForm").attr("action"), $("#manualForm").serialize());
+
+  return false;
+};
+
+// Submits data for the efficient form
+var efficientSubmit = function efficientSubmit(e) {
+  e.preventDefault();
+
+  if ($("#inputValue").val() == '') {
+    handleError("Input is required for submission");
+    return false;
+  }
+
+  // TODO: validation/conversion
+
+  sendAjax($("#efficientForm").attr("action"), $("#efficientForm").serialize());
+
+  return false;
 };
 
 $(document).ready(function () {
-    // Submits data for the manual form
-    $("#manualForm").on("submit", function (e) {
-        e.preventDefault();
+  // Setup default event handler for the manual submission form
+  $("#manualForm").on("submit", manualSubmit);
 
-        if ($("#inputValue").val() == '') {
-            handleError("Input is required for submission");
-            return false;
-        }
+  // Changes between displaying the manual and the efficient forms on the page
+  $("#changeFormButton").click(function () {
+    // console.log("change button clicked");
+    // console.log(manual);
+    if (manual) {
+      $('#changeFormButton').val("Do It the Easy Way");
+      manual = false;
 
-        sendAjax($("#manualForm").attr("action"), $("#manualForm").serialize());
+      $('#input').html("<h1>The Efficient Way:</h1>\n      <form id=\"efficientForm\" name=\"efficientForm\" action=\"/appendValue\" method=\"POST\">\n        <label for=\"inputValue\">Enter 8 Binary Digits: </label>\n        <input id=\"inputValue\" type=\"number\" name=\"inputValue\" value=\"\" size=\"8\">\n        <input class=\"formSubmit\" type=\"submit\" value=\"Send Data\">\n      </form>");
 
-        return false;
-    });
+      // Attach event handler for the efficient form
+      $("#efficientForm").on("submit", efficientSubmit);
+    } else {
+      $('#changeFormButton').val("Do It the Hard Way");
+      manual = true;
 
-    // Submits data for the efficient form
-    $("#efficientForm").on("submit", function (e) {
-        e.preventDefault();
+      $('#input').html("<h1>The Intended Way:</h1>\n      <form id=\"manualForm\" name=\"manualForm\" action=\"/appendValue\" method=\"POST\">\n        <input id=\"zeroButton\" type=\"button\" value=\"0\">\n        <input id=\"oneButton\" type=\"button\" value=\"1\">\n        <label for=\"inputValue\">Enter 8 Binary Digits: </label>\n        <input id=\"inputValue\" type=\"number\" name=\"inputValue\" value=\"00000000\" size=\"8\" disabled>\n        <input class=\"formSubmit\" type=\"submit\" value=\"Send Data\">\n      </form>");
 
-        if ($("#inputValue").val() == '') {
-            handleError("Input is required for submission");
-            return false;
-        }
+      // Attach event handler for the manual form
+      $("#manualForm").on("submit", manualSubmit);
+    }
+  });
 
-        sendAjax($("#efficientForm").attr("action"), $("#efficientForm").serialize());
+  $("#zeroButton").click(function () {
+    console.log("zero clicked");
+    var input = $('#inputValue');
+    var inputValue = input.val();
 
-        return false;
-    });
+    // If length < 8, append
+    if (inputValue.length < 8) {
+      input.val(inputValue.concat("0"));
+    } else {
+      // else, reset the value
+      input.val("");
+    }
+  });
 
-    // Changes between displaying the manual and the efficient forms on the page
-    $("#changeFormButton").on('click', function (e) {
-        $('#changeFormButton').text = "Do It the Hard Way";
-        $('#changeFormButton').text = "Do It the Easy Way";
-    });
+  $("#oneButton").click(function () {
+    console.log("one clicked");
+    var input = $('#inputValue');
+    var inputValue = $('#inputValue').val();
 
-    $("#zeroButton").on('click', function (e) {});
-
-    $("#oneButton").on('click', function (e) {});
+    // If length < 8, append
+    if (inputValue.length < 8) {
+      input.val(inputValue.concat("1"));
+    } else {
+      // else, reset the value
+      input.val("");
+    }
+  });
 });
